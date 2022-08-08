3e9e4c2be39f4ac75f19d518c1316fb5a64698d2
joephy <joephy05@gmail.com>
2022-08-07 23:47:41 -0700

    printf

--- test_files/_printf.c
+++ test_files/_printf.c
@@ -4,16 +4,12 @@
 
 int _printf(const char *format, ...){
   int count = 0, i;
-  
+
   va_list data;
   va_start(data, format);
 
-  /* _printf("%s", 'Hello') */
-  
   for (i = 0; format[i] != '\0'; ){
-    
-    /* count the number of characters */
-    /* print to the screen each character counted */
+
     if (format[i] != '%'){
       count += _putchar(format[i]);
       i++;
--- test_files/_putchar.c
+++ test_files/_putchar.c
@@ -1,5 +1,5 @@
 #include <unistd.h>
-// _putchar - function to return character to stdout
+
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
